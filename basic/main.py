2004a27c8d1a510ce9415f82e176476c35693df3
Edfreak <eduardond7411@gmail.com>
2025-11-14 10:54:23 -0400

    Update

---
+++ basic/main.py
@@ -0,0 +1,11 @@
+apellido = "Garcia"
+nombre = "Juan"
+edad = 30
+ciudad = "Barcelona"
+pais = "España"
+
+ciudades = ["Madrid", "Barcelona", "Valencia", "Sevilla"]
+paises = ["España", "Francia", "Italia", "Alemania"]
+colores = ("rojo", "verde", "azul", "amarillo")
+
+paisCiudad = { "Venezuela": ciudad, "pais": pais }
